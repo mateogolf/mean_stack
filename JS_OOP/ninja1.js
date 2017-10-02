884a42a6d86809d80949c095022f38c35d0e3b69
mateogolf <rodriguezml11@mail.wlu.edu>
2017-10-02 11:46:35 -0400

    Ninja I assignment

---
+++ JS_OOP/ninja1.js
@@ -0,0 +1,29 @@
+function Ninja(name="",health=100){
+    // Create a new class called Ninja with the following attributes:
+    this.name = name;
+    this.health = health;
+    var speed= 3;
+    var strength= 3;
+
+    this.sayName = function(){
+        console.log("My ninja name is " + this.name+"!");
+        return this;
+    }
+    this.showStats = function () {
+        console.log(`Name: ${this.name}, Health: ${this.health},Speed: ${speed},Strength: ${strength}`);
+        return this;
+    }
+    this.drinkSake = function() {
+        this.health+=10;
+        return this;
+    }
+    
+}
+
+var ninja1 = new Ninja("Hyabusa");
+console.log("Test");
+console.log(ninja1.name);
+ninja1.sayName();
+// -> "My ninja name is Hyabusa!"
+ninja1.showStats();
+// -> "Name: Hayabusa, Health: 100, Speed: 3, Strength: 3"
