8be397a7f17a0ccb5508446cabc9941bd5c773f5
mateogolf <rodriguezml11@mail.wlu.edu>
2017-11-01 16:14:03 -0400

    Epic Button Game with SocketIO

---
+++ SocketIO/epic_button/server.js
@@ -0,0 +1,38 @@
+const express = require("express"),
+    path = require("path"),
+    app = express(),
+    // session = require('express-session'),
+    PORT = 8090;
+//Sessions
+// app.use(session({ secret: 'someKey' }));
+//Static and Views
+app.use(express.static(path.join(__dirname, "./static")));
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+// const count=0;
+// Root route to render the index.ejs view.
+app.get('/', function (req, res) {
+    res.render("index")//,{count:count});
+})
+
+// Start Node server listening on port 8000.
+var server = app.listen(PORT, function () {
+    console.log(`listening on port ${PORT}`);
+})
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+    console.log("Client/socket is connected!");
+    console.log("Client/socket id is: ", socket.id);
+    //Code
+    const count=0;
+    socket.on('reset_button',(data)=>{
+        console.log(`Removing count:${data.count}`)
+        socket.emit('reset', { count: 0 })
+    })
+    socket.on('count_button', (data) => {
+        console.log(typeof data.count)
+        socket.emit('counted',{count:data.count+1})
+    })
+
+})
